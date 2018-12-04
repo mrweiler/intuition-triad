d058832cad97f835bb33b5da51fdafec0c68bc27
Matthias Weiler <m.weiler@fu-berlin.de>
2018-12-04 11:07:33 +0100

    add instructions

--- experiment.js
+++ experiment.js
@@ -1,3 +1,26 @@
+/*
+  Experiment
+  author: Matthias Weiler
+  date: 2018-11-30
+*/
+
+/*
+  Instruction block
+  Show instructions on several pages.
+*/
+
+// General instructions
+let instructions = {
+    type: 'instructions',
+    pages: [
+        'First page with instructions',
+        'Second page with instructions',
+        ],
+    show_clickable_nav: true,
+ };
+
+
+// Main
 jsPsych.init({
-      timeline: [],
+      timeline: [instructions],
     });
